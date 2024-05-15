6e4d06a6b08857d7acb146cb909b27e0bcc2a884
cloe.aroca <cloe.aroca@bluetab.net>
2024-05-15 19:49:50 +0200

    WIP: lista compra5

--- src/app/pages/lista_de_la_compra.py
+++ src/app/pages/lista_de_la_compra.py
@@ -10,14 +10,17 @@ path_productos_comprar = "src/app/data/comprar_productos.csv"
 with st.container():
     st.header("Lista de la Compra")
 
-    productos_comprar = pd.read_csv(path_productos_comprar, index_col=0, encoding='utf-8')
+
+    productos_comprar = pd.read_csv(path_productos_comprar, index_col=0, encoding='utf-8')[['id_producto', 'categoría', 'producto', 'marca']]
     if productos_comprar.duplicated().sum() == 0:
-        productos_en_tiendas = pd.read_csv(path_productos_en_t, index_col=0, encoding='utf-8')
+        productos_en_tiendas = pd.read_csv(path_productos_en_t, index_col=0, encoding='utf-8')[['id_producto', 'tienda','uds_en_paquete', 'uds', 'kg', 'precio_ud', 'precio_kg']]
         lista_compra = productos_comprar.merge(productos_en_tiendas, on="id_producto", how="left")
-        logging.info("Lista de la compra calculada!")
+        lista_compra.insert(0, "comprado", [False]*lista_compra.shape[0])
+        print("Lista de la compra calculada!")
 
         st.data_editor(lista_compra, use_container_width=True)
-    logging.info("Hay productos duplicados en tu lista. Revísala bien.")
+    else:
+        logging.info("Hay productos duplicados en tu lista. Revísala bien.")
 
 
 
